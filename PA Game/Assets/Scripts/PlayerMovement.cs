ba761ec3f334bc9b9f8050fe8f96206032dbd54e
Pillow <m.farid.ft20@mail.umy.ac.id>
2023-12-22 23:32:05 +0700

    movement

---
+++ PA Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    //float speed;
+
+    //public Transform orientation;
+
+    float horizontalinput;
+    float verticalinput;
+
+    public float horizontalspeed;
+    public float verticalspeed;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerInput();
+    }
+
+    void PlayerInput()
+    {
+        horizontalinput = Input.GetAxis("Horizontal");
+        verticalinput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * horizontalspeed * Time.deltaTime * horizontalinput);
+        transform.Translate(Vector3.right * verticalspeed * Time.deltaTime * verticalinput);
+
+    }
+}
