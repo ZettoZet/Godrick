a66fefd3785d86844a94f603aacbb786fa28f95e
Pillow <m.farid.ft20@mail.umy.ac.id>
2024-01-08 21:51:18 +0700

    add

--- PA Game/Assets/Scripts/GameManager.cs
+++ PA Game/Assets/Scripts/GameManager.cs
@@ -6,19 +6,71 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public TextMeshProUGUI gameOverText;
-    public TextMeshProUGUI countdownText;
+    /*public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI countdownText;*/
 
-    public Button restartButton;
+    public float countdown = 5f;
+    public static GameManager instance;
+
+    public bool isGameActive;
+
+    /*public Button restartButton;*/
     // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        isGameActive = true;
+        StartCoroutine(Timer());
+
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isGameActive == true)
+        {
+            Countdown();
+        }
+    }
+
+    public void GameOver()
+    {
+        /*gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);*/
+        isGameActive = false;
+
+    }
+
+    public void Countdown()
+    {
+        if(countdown > 0)
+        {
+            countdown -= 1 * Time.deltaTime;
+            /*countdownText.text = "Time: " + Mathf.RoundToInt(countdown);*/
+        }
+        else
+        {
+            isGameActive = false;
+        }
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(countdown);
+
+        if (isGameActive)
+        {
+            GameOver();
+        }
     }
 }
