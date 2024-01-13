1e6daf596176356d4629c5a12f838df6584bb379
Abid Akbar <abidakbar0001@gmail.com>
2024-01-13 17:38:22 +0700

    add high score system

--- PA Game/Assets/Scripts/ScoreScript.cs
+++ PA Game/Assets/Scripts/ScoreScript.cs
@@ -7,42 +7,50 @@ using TMPro;
 public class ScoreScript : MonoBehaviour
 {
     public static ScoreScript instance;
-    public int score;
-    public TextMeshProUGUI scoreText;
+    public int score, highScore;
+    public TextMeshProUGUI scoreText, highScoreText;
 
 
     private void Awake()
     {
         instance = this;
-        
+
     }
     // Start is called before the first frame update
     void Start()
     {
-
+        loadHighScore();
     }
 
     // Update is called once per frame
     void Update()
     {
         scoreText.text = "Score: " + score.ToString();
+        highScoreText.text = "High Score: " + highScore.ToString();
         //Debug.Log("Score : " + score);
         //LoadScore();
+
     }
 
     public void AddScore()
     {
         score += 10;
+        if (score > highScore)
+        {
+            highScore = score;
+        }
         SaveScore();
     }
 
-    public void LoadScore()
+    public void loadHighScore()
     {
-       score = PlayerPrefs.GetInt("Score", 0);
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
     }
 
     public void SaveScore()
     {
         PlayerPrefs.SetInt("Score", score);
+        PlayerPrefs.SetInt("HighScore", highScore);
+        PlayerPrefs.Save();
     }
 }
