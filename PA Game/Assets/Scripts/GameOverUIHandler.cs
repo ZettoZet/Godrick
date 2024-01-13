78e7a8843f5d4b180070035a2a1b461c9b068d25
Abid Akbar <abidakbar0001@gmail.com>
2024-01-14 01:35:57 +0700

    set method GoToMainMenu() to public

--- PA Game/Assets/Scripts/GameOverUIHandler.cs
+++ PA Game/Assets/Scripts/GameOverUIHandler.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameOverUIHandler : MonoBehaviour
 {
     // Start is called before the first frame update
-    void GoToMainMenu()
+    public void GoToMainMenu()
     {
         SceneManager.LoadScene("Main Menu");
     }
